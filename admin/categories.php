4971e0c8747e76f8e9dafb2f9daf71096fe592d7
szyth <syedzia.pro@gmail.com>
2020-08-04 20:31:24 +0530

    late commit

---
+++ admin/categories.php
@@ -0,0 +1,95 @@
+<?php
+require('top.inc.php');
+
+if (isset($_GET['type']) && $_GET['type'] != '') {
+    $type = get_safe_value($con, $_GET['type']);
+   
+    if ($type == 'status') {
+
+        $operation = get_safe_value($con, $_GET['operation']);
+        $id = get_safe_value($con, $_GET['id']);
+
+        if ($operation == 'active') {
+            $status = '1';
+        } 
+        else if ($operation == 'deactive')   {
+            $status = '0';
+        }
+        else{
+            echo "Wrong Input";
+        }
+
+        $sql_update_status = "UPDATE categories SET status='$status' WHERE id='$id'";
+        mysqli_query($con, $sql_update_status);
+    }
+    if ($type == "delete") {
+        $id = get_safe_value($con, $_GET['id']);
+        $sql_delete_status = "DELETE FROM categories WHERE id='$id'";
+        mysqli_query($con, $sql_delete_status);
+    }
+}
+
+
+$sql = "SELECT * FROM categories ORDER BY categories ASC";
+$res = mysqli_query($con, $sql);
+?>
+
+
+<div class="content pb-0">
+    <div class="orders">
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="box-title">Categories </h4>
+                        <h4 class="box-link"><a href="manage_categories.php"> Add Categories </a></h4>
+                    </div>
+                    <div class="card-body--">
+                        <div class="table-stats order-table ov-h">
+                            <table class="table ">
+                                <thead>
+                                    <tr>
+                                        <th class="serial">#</th>
+                                        <th>ID</th>
+                                        <th>Categories</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $i = 1;
+                                    while ($row = mysqli_fetch_assoc($res)) {
+                                    ?>
+                                        <tr>
+                                            <td class="serial"><?php echo $i ?></td>
+                                            <td><?php echo $row['id'] ?></td>
+                                            <td><?php echo $row['categories'] ?></td>
+                                            <td>
+                                                <?php
+                                                if ($row['status'] == 1) {
+                                                    echo " <a href='?type=status&operation=deactive&id=" . $row['id'] . "'><span class='badge badge-complete'>Active</span></a>&nbsp;";
+                                                } else {
+                                                    echo " <a href='?type=status&operation=active&id=" . $row['id'] . "'><span class='badge badge-pending'>Deactive</span></a>&nbsp;";
+                                                }
+                                                echo "<a href='manage_categories.php?id=" . $row['id'] . "'><span class='badge badge-primary'>Edit</span></a>&nbsp;&nbsp;";
+
+                                                echo "<a href='?type=delete&id=" . $row['id'] . "'><span class='badge badge-danger'>Delete</span></a>";
+                                                ?>
+                                            </td>
+                                        </tr>
+                                    <?php
+                                    }
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+require('footer.inc.php');
+?>
---
+++ admin/manage_categories.php
@@ -0,0 +1,61 @@
+<?php
+require('top.inc.php');
+
+$categories = '';
+if(isset($_GET['id']) && $_GET['id']!='') {
+    $id = get_safe_value($con,$_GET['id']);
+    $sql = "SELECT * FROM categories WHERE id = '$id'";
+    $res = mysqli_query($con,$sql);
+    $check = mysqli_num_rows($res);
+    if($check>0){
+        $row = mysqli_fetch_assoc($res);
+        $categories = $row['categories'];
+    }
+    else {
+        header('location:categories.php');
+        die();
+    }
+    
+}
+if(isset($_POST['submit'])){
+    $categories = get_safe_value($con,$_POST['categories']);
+
+    if(isset($_GET['id']) && $_GET['id']!='') {
+        $sql = "UPDATE categories SET categories='$categories' WHERE id='$id'";
+
+    }
+    else {
+        $sql = "INSERT INTO categories(categories,status) VALUES ('$categories', '1')";
+
+    }
+    mysqli_query($con, $sql);
+    header('location:categories.php');
+    die();  
+}
+?>
+<div class="content pb-0">
+    <div class="animated fadeIn">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-header"><strong>Categories</strong><small> Form</small></div>
+                    <form action="" method="post">
+                        <div class="card-body card-block">
+                            <div class="form-group">
+                                <label for="categories" class="form-control-label">Category</label>
+                                <input type="text" name="categories" placeholder="Enter Category name" value="<?php echo $categories?>" class="form-control" required>
+                            </div>
+
+                            <button type="submit" name="submit" class="btn btn-lg btn-info btn-block">
+                                <span id="payment-button-amount">Submit</span>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+require('footer.inc.php');
+?>
