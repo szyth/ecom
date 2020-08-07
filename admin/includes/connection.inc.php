a211d2507e98b148b845ac434da33d0ea15cccf3
szyth <syedzia.pro@gmail.com>
2020-08-07 19:07:01 +0530

    Refactored Connection.inc.php

--- admin/includes/connection.inc.php
+++ admin/includes/connection.inc.php
@@ -1,11 +1,8 @@
 <?php
 
 session_start();
-$con = mysqli_connect("localhost", "id14556290_root","Ziaur@8574803737","id14556290_ecom");
-
-define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/ecom/');
-define('SITE_PATH','http://localhost/ecom/');
-
-
-define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
-define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
+$con = mysqli_connect("localhost", "root", "", "ecom");
+// $con = mysqli_connect("localhost", "id14556290_root", "Ziaur@8574803737", "id14556290_ecom");
+if (mysqli_connect_errno()) {
+    echo "Connection could not be established..." . mysqli_connect_error();
+}
--- includes/connection.inc.php
+++ includes/connection.inc.php
@@ -1,11 +1,10 @@
 <?php
 
 session_start();
-$con = mysqli_connect("localhost", "id14556290_root", "Ziaur@8574803737", "id14556290_ecom");
+$con = mysqli_connect("localhost", "root", "", "ecom");
+// $con = mysqli_connect("localhost", "id14556290_root", "Ziaur@8574803737", "id14556290_ecom"); FOR obb-ecom 000WEBHOST
 
-define('SERVER_PATH', $_SERVER['DOCUMENT_ROOT'] . '/ecom/');
-define('SITE_PATH', 'http://localhost/ecom/');
+if(mysqli_connect_errno()){
+    echo "Connection could not be established...".mysqli_connect_error();
+}
 
-
-define('PRODUCT_IMAGE_SERVER_PATH', SERVER_PATH . 'media/product/');
-define('PRODUCT_IMAGE_SITE_PATH', SITE_PATH . 'media/product/');
