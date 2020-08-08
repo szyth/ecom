f61ea09f098040accb95c135a3eb7e8b6e7f3c48
szyth <syedzia.pro@gmail.com>
2020-08-08 12:40:49 +0530

    Add to Cart add

--- js/main.js
+++ js/main.js
@@ -540,3 +540,15 @@ function user_login() {
     });
   }
 }
+
+function manage_cart(pid, type) {
+  var qty = jQuery("#qty").val();
+  jQuery.ajax({
+    url: "manage_cart.php",
+    type: "post",
+    data: "pid=" + pid + "&qty=" + qty + "&type=" + type,
+    success: function (result) {
+      jQuery('.htc__qua').html(result);
+    },
+  });
+}
--- product.php
+++ product.php
@@ -63,13 +63,32 @@ if ($product_id > 0) {
                             <div class="sin__desc">
                                 <p><span>Availability:</span> In Stock</p>
                             </div>
+                            <div class="sin__desc">
+                                <p><span>Quantity:</span>
+                                    <select id="qty">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                        <option>6</option>
+                                        <option>7</option>
+                                        <option>8</option>
+                                        <option>9</option>
+                                        <option>10</option>
+                                    </select>
+
+                                </p>
+                            </div>
                             <div class="sin__desc align--left">
                                 <p><span>Categories:</span></p>
                                 <ul class="pro__cat__list">
                                     <li><a href="#"><?php echo $get_product['0']['categories'] ?></a></li>
                                 </ul>
                             </div>
-                            <a class="fr__btn" href="#">Add to Cart</a>
+
+                            <a class="fr__btn" href="javascript:void(0)" onclick="manage_cart('<?php echo $get_product['0']['id'] ?>','add')">Add to cart</a>
+
                         </div>
                     </div>
                 </div>
