7e2a2a4f61797b44e784e81653f20dd997e9f99d
szyth <syedzia.pro@gmail.com>
2020-08-14 20:20:14 +0530

    Contact page added; Minor Refactoring

--- front-end/js/init.js
+++ front-end/js/init.js
@@ -17,7 +17,20 @@ $(document).ready(function () {
 setInterval(function () {
   $(".carousel").carousel("next");
 }, 2000); // every 2 seconds
-//TABS
-$(document).ready(function(){
-  $('.tabs').tabs();
-});
+
+
+var myCenter = new google.maps.LatLng(51.308742, -0.32085);
+function initialize() {
+  var mapProp = {
+    center: myCenter,
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+  };
+  var map = new google.maps.Map(document.getElementById("map"), mapProp);
+
+  var marker = new google.maps.Marker({
+    position: myCenter,
+  });
+  marker.setMap(map);
+}
+google.maps.event.addDomListener(window, "load", initialize);
