cd4011d427b0f4118f161dcdf48020bb6523b85e
szyth <syedzia.pro@gmail.com>
2020-08-16 12:16:26 +0530

    Filter section added in categories

--- front-end/js/init.js
+++ front-end/js/init.js
@@ -22,9 +22,33 @@ $(document).ready(function () {
   $(".tabs").tabs();
 });
 
+//card color swap
 $("#register_click").click(function () {
   $(".card").addClass("register-card");
 });
 $("#login_click").click(function () {
   $(".card").removeClass("register-card");
+});
+
+//colored button check box
+$(".btn-floating").click(function () {
+  if ($(this).find("i").text() == "") {
+    $(this).find("i").text("done");
+  } else {
+    $(this).find("i").text("");
+  }
+});
+
+//toggle filter menu
+$("#price").click(function () {
+  $("#price_body").toggle();
+});
+$("#size").click(function () {
+  $("#size_body").toggle();
+});
+$("#type").click(function () {
+  $("#type_body").toggle();
+});
+$("#fabric").click(function () {
+  $("#fabric_body").toggle();
 });
