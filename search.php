a3364d2f54a5b3c39ee1342ec31ef76e1bf9aee5
szyth <syedzia.pro@gmail.com>
2020-08-25 11:46:01 +0530

    Search bar init

---
+++ search.php
@@ -0,0 +1,37 @@
+<?php require('includes/header.inc.php'); ?>
+
+
+<section>
+    <div class="parallax-container valign-wrapper">
+        <div class="section no-pad-bot">
+            <div class="container">
+                <div class="row center">
+                    <h5 class="header col s12">
+                        <div class="breadcrumb_wrapper">
+                            <a href="index.php" class="breadcrumb">Home</a>
+                            <a class="breadcrumb">Search Products</a>
+                        </div>
+                    </h5>
+                </div>
+            </div>
+        </div>
+        <div class="parallax"><img src="media/parallax/2.jpg" alt="Unsplashed background img 2"></div>
+    </div>
+</section>
+
+
+
+
+
+
+<section style="background-color: rgb(255, 255, 255);">
+    <svg class="curve" data-name="layer" viewBox="0 0 1416.9 174.01">
+        <path d="M0,220.8S283.66,120,608.94,163.56s437.93,100.57,818,10.34V309.54H0V280.8Z" transform="translate(0 -120.53)" />
+
+    </svg>
+</section>
+
+
+
+
+<?php require('includes/footer.inc.php'); ?>
