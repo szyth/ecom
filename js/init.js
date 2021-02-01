b86186bd880f7a9e9626a13f08d93387553106ea
szyth <syedzia.pro@gmail.com>
2021-02-01 16:38:20 +0530

    Product display now includes other similar variants

--- js/init.js
+++ js/init.js
@@ -427,3 +427,33 @@ $(document).ready(function () {
 
   })
 });
+
+
+// PRODUCT SLIDER 
+$(document).ready(function () {
+  $(".tb").hover(function () {
+    $(".tb").removeClass("tb-active");
+    $(this).addClass("tb-active");
+
+    current_fs = $(".active");
+
+    next_fs = $(this).attr("id");
+    next_fs = "#" + next_fs + "1";
+
+    $("fieldset").removeClass("active");
+    $(next_fs).addClass("active");
+
+    current_fs.animate({}, {
+      step: function () {
+        current_fs.css({
+          display: "none",
+          position: "relative",
+        });
+        next_fs.css({
+          display: "block",
+        });
+      },
+    });
+  });
+});
+// PRODUCT SLIDER - END
